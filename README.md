27eb633af5d14a2b2e03d40fba8a05cf69c5dec2
Shaurya Pandey <shauryapandey40@gmail.com>
2026-03-25 23:25:34 +0530

    Completed the city repository

--- README.md
+++ README.md
@@ -23,6 +23,8 @@
 
    - Once you have added db config as listed above , go to the src folder from your terminal and execute `npx sequelize db:create`
 
+   and then execute `npx sequelize db:migrate`
+
 
 
 
--- src/repository/city-repo.js
+++ src/repository/city-repo.js
@@ -8,11 +8,13 @@ const { City } = require('../models/index');
 
 
 class CityRepo {
+  //directly access the name key 
+  //instead of obj.key 
   async createCity({ name }) {
     try {
       return await City.create({ name });
     } catch (error) {
-      console.error(error);
+      console.log("Something went wrong in the repo layer");
       throw error;
     }
   }
@@ -23,13 +25,43 @@ class CityRepo {
         where: { id: cityid }
       });
     } catch (error) {
+      console.log("Something went wrong in the repo layer");
       throw error;
     }
   }
+  
+        //which city   what we want to update
+        //data will be key value pair 
+        //what we want to update and with which value 
+  async updateCity(cityid , data) {
+     try {
+       const city = await City.update(data,{
+        where: {
+           id : cityid
+        }
+       });
+       return city;
+    } catch (error) {
+      console.log("Something went wrong in the repo layer");
+      throw error;
+    }
+  }
+
+
+
+  async getCity(cityid) {
+    try {
+      return await City.findByPk(cityid); 
+    } catch (error) {
+      console.log("Something went wrong in the repo layer");
+      throw error;
+    }
+  }
+
 }
 
 
 module.exports = CityRepo;
 //ab ye hm export kr rhe to ise hm khi or import kr skte h is class ko
 //agr file/folder hota to hme require krne s ho jata 
-//per ye part of code to import/export 
+//per ye part of code to import/export  

6df894cac0d72d375eebfb540dc2dfd82c1c682d
Shaurya Pandey <shauryapandey40@gmail.com>
2026-03-31 01:41:31 +0530

    Added the error code enums

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -1,3 +1,5 @@
+const {ClientErrorsCodes,ServerErrorsCodes,SuccessCodes} = require('../utils/error-codes');
+
 const {FlightService} = require('../services/index');
 
 const flightService = new FlightService();
@@ -14,7 +16,7 @@ const create = async (req,res)=>{
           price:req.body.price
      } 
         const flight = await flightService.createFlight(flightReqData);
-        return res.status(201).json({
+        return res.status(SuccessCodes.CREATED).json({
              data : flight,
               success : true,
               message : "Successfullly created the Flight",
@@ -34,7 +36,7 @@ const create = async (req,res)=>{
 const getAll = async(req,res)=>{
        try {
             const resp = await flightService.getAllFlightData(req.query);
-            return res.status(200).json({
+            return res.status(SuccessCodes.OK).json({
               data : resp,
               success : true,
               message : "Successfullly Fetched the Flights",
--- src/middleware/flight-middleware.js
+++ src/middleware/flight-middleware.js
@@ -1,3 +1,6 @@
+
+const {ClientErrorsCodes,ServerErrorsCodes,SuccessCodes} = require('../utils/error-codes'); 
+
 const validateCreateFlight = (req,res,next) => {
     if(
         !req.body.flightNumber ||
@@ -8,7 +11,7 @@ const validateCreateFlight = (req,res,next) => {
         !req.body.departureTime ||
         !req.body.price   
     ){ //if any of the body params is missing
-        return res.status(400).json({
+        return res.status(ClientErrorsCodes.BAD_REQUEST).json({
             data : {},
             success : false,
             message : "Invalid Body params",
